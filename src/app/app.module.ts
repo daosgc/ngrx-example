ced2c33781feab4a3815233712b828aa9dd62228
Darwin Gualotuña <daosgc@gmail.com>
2020-04-06 15:17:00 -0500

    Add pokemon api and show pokemon list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { StoreModule } from '@ngrx/store';
 import { CustomerReducer } from './reducers/customer.reducer';
 import { CustomersViewComponent } from './pages/customers-view/customers-view.component';
 import { CustomerAddComponent } from './pages/customer-add/customer-add.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomersViewComponent,
-    CustomerAddComponent
+    CustomerAddComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ customers: CustomerReducer })
+    StoreModule.forRoot({ customers: CustomerReducer }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
